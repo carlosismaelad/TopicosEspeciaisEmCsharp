f61978ae2a93428b4cc0311f11fe19a92f40a812
carlosismaelad <carlosismaelad@outlook.com>
2023-09-05 11:52:18 -0300

    Adicionando tempo às datas

---
+++ DateTimeAddvalues/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace DateTimeAddValues
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Clear();
+
+            var date = DateTime.Now;
+
+            Console.WriteLine(date.AddDays(30));
+
+            Console.WriteLine(date.AddMonths(3));
+
+            Console.WriteLine(date.AddYears(1));
+
+            /* 
+            É aconselhado optar por esses métodos sempre, em vez de realizar adição ou subtração de datas manualmente
+            através de atribuição.
+             */
+
+
+        }
+    }
+
+}

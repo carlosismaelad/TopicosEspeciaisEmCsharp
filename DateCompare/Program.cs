e5ef85ee994a9503ff1263194eb3f9fe2ae5d409
carlosismaelad <carlosismaelad@outlook.com>
2023-09-05 12:04:35 -0300

    Comparando datas de forma correta

---
+++ DateCompare/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace DateCompare
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Clear();
+
+            DateTime? date = null; // usando o tipo DateTime e o "?" para podermos ter uma data que pode ser nula
+            Console.WriteLine(date);
+
+            // Comparação de datas de forma INCORRETA:
+            DateTime data = DateTime.Now;
+
+            if (data == DateTime.Now) // nunca será porque o Dotnet compara a estrutura toda e os milissegundos sempre serão diferentes
+            {
+                Console.WriteLine("É igual!");
+            }
+            else
+            {
+                Console.WriteLine("Não é igual"); // esta será a saída
+            }
+
+            // Comparando de forma CORRETA:
+            if (data.Date == DateTime.Now.Date)
+            {
+                Console.WriteLine("É igual"); // a saída será esta
+            }
+            else
+            {
+                Console.WriteLine("É diferente");
+            }
+
+
+
+
+
+        }
+    }
+}

47278762cb7ec9930e4efd58e866efbafda0bed4
carlosismaelad <carlosismaelad@outlook.com>
2023-09-06 12:28:36 -0300

    Usando TimeZoneInfo, FindSystemTimeZoneById e ConvertTimeFromUtc para conversões e iterando sobre todos os TimeZones

---
+++ IntroTimeZone/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace IntroTimeZone
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Clear();
+
+            var utcDate = DateTime.UtcNow; // instancia o presente momento com o padrão Utc
+            Console.WriteLine(utcDate);
+
+            Console.WriteLine(utcDate.ToLocalTime()); // fazendo o parse para a hora local
+
+            var timeZoneAustralia = TimeZoneInfo.FindSystemTimeZoneById("Pacific/Auckland");
+            Console.WriteLine(timeZoneAustralia); // (UTC+12:00) Horário da Nova Zelândia (Auckland)
+
+            var horaAustralia = TimeZoneInfo.ConvertTimeFromUtc(utcDate, timeZoneAustralia);
+            Console.WriteLine(horaAustralia);
+
+
+            /* Como listar todos os TimeZones */
+            var timeZones = TimeZoneInfo.GetSystemTimeZones();
+
+            foreach (var timeZone in timeZones)
+            {
+                Console.WriteLine(timeZone.Id);
+                Console.WriteLine(timeZone);
+                Console.WriteLine(TimeZoneInfo.ConvertTimeFromUtc(utcDate, timeZone));
+                Console.WriteLine("----------");
+            }
+
+
+
+
+
+
+
+        }
+    }
+}

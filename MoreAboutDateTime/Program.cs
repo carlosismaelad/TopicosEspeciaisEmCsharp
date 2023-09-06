06df6b43135202bf9002dd3b85f7832b86a7d4c8
carlosismaelad <carlosismaelad@outlook.com>
2023-09-06 12:49:14 -0300

    Outras funcionalidades muito usadas no DateTime

---
+++ MoreAboutDateTime/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Reflection.Metadata;
+
+namespace MoreAboutDateTime
+{
+    class program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Clear();
+
+            Console.WriteLine(DateTime.DaysInMonth(2023, 2)); // quantos dias tem Fevereiro em 2023
+            Console.WriteLine(IsWeekend(DateTime.Now.DayOfWeek)); // Retorna True ou False informando se hoje é fim de semana
+            Console.WriteLine(DateTime.Now.IsDaylightSavingTime()); // Responde true ou False informando se estamos em horário de verão
+
+        }
+
+        static bool IsWeekend(DayOfWeek today)
+        {
+            return today == DayOfWeek.Sunday || today == DayOfWeek.Saturday;
+        }
+    }
+}

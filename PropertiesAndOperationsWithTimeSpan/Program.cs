0eaf1e83f677a74a584f9631c4de32d602a9778d
carlosismaelad <carlosismaelad@outlook.com>
2023-09-06 11:24:55 -0300

    Propriedades e operações com TimeSpan

---
+++ PropertiesAndOperationsWithTimeSpan/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace PropertiesAndOperationsWithTimeSpan
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            TimeSpan t = new TimeSpan(2, 3, 5, 7, 11);
+
+            Console.WriteLine(t);
+            Console.WriteLine("Days: " + t.Days);
+            Console.WriteLine("Hours: " + t.Hours);
+            Console.WriteLine("Minutes: " + t.Minutes);
+            Console.WriteLine("Milliseconds: " + t.Milliseconds);
+            Console.WriteLine("Seconds: " + t.Seconds);
+            Console.WriteLine("Ticks: " + t.Ticks);
+            Console.WriteLine("TotalDays: " + t.TotalDays);
+            Console.WriteLine("TotalHours: " + t.TotalHours);
+            Console.WriteLine("TotalMinutes: " + t.TotalMinutes);
+            Console.WriteLine("TotalSeconds: " + t.TotalSeconds);
+            Console.WriteLine("TotalMilliseconds: " + t.TotalMilliseconds);
+        }
+    }
+}
